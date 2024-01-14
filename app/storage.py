115dfef83ba0b25006dc9d4de73c912abab1b713
reydotcom <rey.dot.conn@gmail.com>
2024-01-14 21:21:06 +0100

    A module for working with the file system has been created

---
+++ app/storage.py
@@ -0,0 +1,20 @@
+import shutil
+import os
+
+from app.config import storage
+
+
+def delete_storage(dir_name):
+    shutil.rmtree(dir_name)
+
+
+def create_storage(name, index):
+    w_dir = f'{storage}/{name}/'
+    if os.path.isdir(w_dir) is not True:
+        os.mkdir(w_dir)
+
+    w_dir += index
+    if os.path.isdir(w_dir) is not True:
+        os.mkdir(w_dir)
+
+    return w_dir
