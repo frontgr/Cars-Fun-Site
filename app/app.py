3989629f5b96302b09ca376dd5631a325edf59cc
reydotcom <rey.dot.conn@gmail.com>
2024-01-14 21:39:25 +0100

    The router "hello" was deleted, and the import paths were modified due to the relocation of routers to the "routers" folder

--- app/app.py
+++ app/app.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_login import LoginManager
 
-from config import secret_key
+from .config import secret_key
+from .models.admin import Admin
 
-from models import Admin
-
-from auth import auth
-from panel import panel
-
+from .routers.main import main
+from .routers.auth import auth
+from .routers.panel import panel
 
 app = Flask(__name__)
 app.secret_key = secret_key
@@ -18,13 +17,9 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return Admin(id=user_id)
+    return Admin(_id=user_id)
 
 
-@app.route('/')
-def hello():
-    return render_template('base.html')
-
-
+app.register_blueprint(main)
 app.register_blueprint(auth)
 app.register_blueprint(panel)
