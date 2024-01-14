0ddeccc774b5b7e0abeafc4f1b6bbb66114bc581
reydotcom <rey.dot.conn@gmail.com>
2024-01-14 21:31:25 +0100

    Data about the storage has been added

--- app/config.py
+++ app/config.py
@@ -6,6 +6,10 @@ load_dotenv()
 # Flask app
 secret_key = os.getenv('SECRET_KEY')
 
+# Storage
+storage = './app/storage'
+extensions = {'png', 'jpg', 'jpeg'}
+
 # DataBase
 host = os.getenv('HOST')
 port = os.getenv('PORT')
