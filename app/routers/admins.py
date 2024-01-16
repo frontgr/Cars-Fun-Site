b9879d6168ed4b68b7f2b939f7e918575cc6c494
reydotcom <rey.dot.conn@gmail.com>
2024-01-16 23:52:09 +0100

    Routers were created to handle administrators

--- app/routers/admins.py
+++ app/routers/admins.py
@@ -1,12 +1,46 @@
 from flask import Blueprint, request, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 
-from app.models.cars import Cars
+from app.models.admins import Admins
+from app.models.admin import admin_permission
+
 
 admins = Blueprint('admins', __name__)
 
 
 @admins.route('/panel/admins', methods=['POST'])
 @login_required
-def panel_post():
-    pass
+def admins_post():
+    return jsonify(Admins().get_admins())
+
+
+@admins.route('/panel/add_admin', methods=['POST'])
+@login_required
+@admin_permission(current_user, 'add_users')
+def admins_add_post():
+    values_dict = {i: request.form.get(i) for i in request.form}
+
+    response = Admins().add_admin(values=values_dict)
+    return response
+
+
+@admins.route('/panel/delete_admin', methods=['DELETE'])
+@login_required
+@admin_permission(current_user, 'delete_users')
+def delete_admin():
+    _id = request.args.get('_id')
+
+    response = Admins().delete_admin(_id)
+    return response
+
+
+@admins.route('/panel/update_admin', methods=['POST'])
+@login_required
+@admin_permission(current_user, 'edit_users')
+def admin_update_post():
+    _id = request.args.get('_id')
+    values = {i: request.form.get(i) for i in request.form if i != 'password'}
+
+    response = Admins().update_admin(_id=_id,
+                                     values_dict=values)
+    return response
