a2801ef9ff7f4d65f28e2144d4bbb0cf3a366057
reydotcom <rey.dot.conn@gmail.com>
2024-01-22 18:47:43 +0100

    The allowed_file decorator has been moved to a separate file with decorators

--- app/converter.py
+++ app/converter.py
@@ -1,22 +1,6 @@
 from PIL import Image
 
-from app.config import extensions
 from app.storage import create_storage
-
-
-def allowed_file(func):
-    def wrapper_allowed_file(*args, **kwargs):
-        filename_list = kwargs['photos'] if 'photos' in kwargs else args[1]['photos']
-        if not filename_list:
-            return func(*args, **kwargs)
-
-        for file in filename_list.values():
-            if file.filename.rsplit('.')[1] not in extensions:
-                return {'status': 'Error',
-                        'message': 'Invalid photos extension'}
-            return func(*args, **kwargs)
-
-    return wrapper_allowed_file
 
 
 def convert(filename, index, file):
