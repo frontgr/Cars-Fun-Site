60aaf1fecd3a59c40e0ce210ca2f4b05f3d36031
reydotcom <rey.dot.conn@gmail.com>
2024-01-22 20:44:51 +0100

    Sets containing lists of available fields in models have been added

--- app/config.py
+++ app/config.py
@@ -17,3 +17,34 @@ port = os.getenv('PORT')
 
 db_uri = f"mongodb://{host}:{port}/"
 
+# Models
+add_admin_fields = {'login',
+                    'password',
+                    'add_users',
+                    'edit_users',
+                    'delete_users',
+                    'add_cars',
+                    'edit_cars',
+                    'delete_cars'}
+update_admin_fields = {'_id',
+                       'add_users',
+                       'edit_users',
+                       'delete_users',
+                       'add_cars',
+                       'edit_cars',
+                       'delete_cars'}
+
+add_car_fields = {'name',
+                  'number',
+                  'type',
+                  'speed_up',
+                  'max_speed',
+                  'description',
+                  }
+update_car_fields = {'_id',
+                     'number',
+                     'type',
+                     'speed_up',
+                     'max_speed',
+                     'description',
+                     }
