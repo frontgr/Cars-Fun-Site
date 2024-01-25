02f54228dcec2ef0eae13c57b460711c36636c85
reydotcom <rey.dot.conn@gmail.com>
2024-01-25 19:12:21 +0100

    refactor: In the login_post function, the issuance of the JWT token has been modified, and the token is now stored in a cookie

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request
-from flask_jwt_extended import create_access_token
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token, set_access_cookies
 
 from app.models.admin import Admin
 
@@ -16,5 +16,8 @@ def login_post():
     if admin.data_validation(password) is False:
         return {'status': 'Error'}
 
-    token = create_access_token(identity=admin.get_id())
-    return {'status': 'ok', 'access_token': token}
+    response_data = jsonify({"msg": "login successful"})
+    access_token = create_access_token(identity=admin.get_id())
+    set_access_cookies(response_data, access_token)
+
+    return response_data
