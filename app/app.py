0a550ddd1d78764985356802cc6f1b592ce7c3a9
reydotcom <rey.dot.conn@gmail.com>
2024-01-25 19:13:42 +0100

    refactor: The refresh_expiring_jwts function has been moved to a separate file

--- app/app.py
+++ app/app.py
@@ -1,11 +1,10 @@
-from datetime import datetime
 from datetime import timedelta
-from datetime import timezone
 
 from flask import Flask
-from flask_jwt_extended import JWTManager, get_jwt, create_access_token, get_jwt_identity, set_access_cookies
+from flask_jwt_extended import JWTManager
 
 from .config import secret_key, jwt_secret_key
+from .response_handlers import refresh_expiring_jwts
 
 from .models.admin import Admin
 
@@ -24,28 +23,19 @@ app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
 jwt = JWTManager(app)
 
 
+@app.after_request
+def after_request_handler(response):
+    return refresh_expiring_jwts(response)
+
+
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
     return Admin(_id=identity)
 
 
-@app.after_request
-def refresh_expiring_jwts(response):
-    try:
-        exp_timestamp = get_jwt()["exp"]
-        now = datetime.now(timezone.utc)
-        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
-        if target_timestamp > exp_timestamp:
-            access_token = create_access_token(identity=get_jwt_identity())
-            set_access_cookies(response, access_token)
-        return response
-    except (RuntimeError, KeyError):
-        return {'status': 'Error',
-                'message': 'Error during the update of the JWT token'}
-
-
 app.register_blueprint(main)
 app.register_blueprint(auth)
 app.register_blueprint(panel)
 app.register_blueprint(admins)
+
