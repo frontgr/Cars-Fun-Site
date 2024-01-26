3000146ed516eaa46eb7acd03273787276c50e4d
reydotcom <rey.dot.conn@gmail.com>
2024-01-26 17:49:35 +0100

    fix: A cookie with a CSRF token has been added after registration

--- back/app/routers/auth.py
+++ back/app/routers/auth.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, set_access_cookies
+from flask_wtf.csrf import generate_csrf
 
 from app.models.admin import Admin
+
 
 auth = Blueprint('auth', __name__)
 
@@ -18,6 +20,9 @@ def login_post():
 
     response_data = jsonify({"msg": "login successful"})
     access_token = create_access_token(identity=admin.get_id())
+    csrf_token = generate_csrf()
+
     set_access_cookies(response_data, access_token)
+    response_data.set_cookie('csrf_access_token', csrf_token)
 
     return response_data
