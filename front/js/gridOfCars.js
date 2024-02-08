c62bc4847300645d30407df701146bb9d4cae38a
nibezo <nibezo.cs@gmail.com>
2024-02-08 12:01:25 +0400

    style: Update popup appearance

---
+++ front/js/gridOfCars.js
@@ -0,0 +1,6 @@
+const photos = document.querySelector(".main__photos").children;
+const photosCount = photos.length;
+for (let i = 0; i < photosCount; i++) {
+  const photo = photos[i];
+  console.log(photo);
+}
