67ce04f2da974d71ed84e1b6f8ba5897f88b8669
Aleksey02 <lyosha2002stich@gmail.com>
2024-03-23 13:21:43 +0500

    feat: Add logic to showmore and hide btns

--- front/js/cars_tab.js
+++ front/js/cars_tab.js
@@ -1,64 +1,64 @@
 const cars = [
-  {
-    name: "Lightning McQueen",
-    number: 95,
-    type: "racers",
-    photo: "./images/main_item_1.webp",
-  },
-  {
-    name: "Jackson storm",
-    type: "racers",
-    number: 20,
-    photo: "./images/main_item_3.webp",
-  },
-  {
-    name: "Lewis Hamilton",
-    type: "racers",
-    photo: "./images/main_item_4.webp",
-  },
-  {
-    name: "Metr",
-    type: "other",
-    photo: "./images/main_item_5.webp",
-  },
-  {
-    name: "Luigi",
-    type: "other",
-    photo: "./images/main_item_6.webp",
-  },
-  {
-    name: "Cruz Ramirez",
-    type: "racers",
-    photo: "./images/main_item_2.webp",
-  },
-  {
-    name: "Lightning McQueen",
-    number: 95,
-    type: "racers",
-    photo: "./images/main_item_1.webp",
-  },
-  {
-    name: "Cruz Ramirez",
-    type: "racers",
-    photo: "./images/main_item_2.webp",
-  },
-  {
-    name: "Lightning McQueen",
-    number: 95,
-    type: "racers",
-    photo: "./images/main_item_1.webp",
-  },
-  {
-    name: "Cruz Ramirez",
-    type: "racers",
-    photo: "./images/main_item_2.webp",
-  },
-  {
-    name: "Lightning McQueen",
-    number: 95,
-    type: "racers",
-    photo: "./images/main_item_1.webp",
-  },
+  // {
+  //   name: "Lightning McQueen",
+  //   number: 95,
+  //   type: "racers",
+  //   photo: "./images/main_item_1.webp",
+  // },
+  // {
+  //   name: "Jackson storm",
+  //   type: "racers",
+  //   number: 20,
+  //   photo: "./images/main_item_3.webp",
+  // },
+  // {
+  //   name: "Lewis Hamilton",
+  //   type: "racers",
+  //   photo: "./images/main_item_4.webp",
+  // },
+  // {
+  //   name: "Metr",
+  //   type: "other",
+  //   photo: "./images/main_item_5.webp",
+  // },
+  // {
+  //   name: "Luigi",
+  //   type: "other",
+  //   photo: "./images/main_item_6.webp",
+  // },
+  // {
+  //   name: "Cruz Ramirez",
+  //   type: "racers",
+  //   photo: "./images/main_item_2.webp",
+  // },
+  // {
+  //   name: "Lightning McQueen",
+  //   number: 95,
+  //   type: "racers",
+  //   photo: "./images/main_item_1.webp",
+  // },
+  // {
+  //   name: "Cruz Ramirez",
+  //   type: "racers",
+  //   photo: "./images/main_item_2.webp",
+  // },
+  // {
+  //   name: "Lightning McQueen",
+  //   number: 95,
+  //   type: "racers",
+  //   photo: "./images/main_item_1.webp",
+  // },
+  // {
+  //   name: "Cruz Ramirez",
+  //   type: "racers",
+  //   photo: "./images/main_item_2.webp",
+  // },
+  // {
+  //   name: "Lightning McQueen",
+  //   number: 95,
+  //   type: "racers",
+  //   photo: "./images/main_item_1.webp",
+  // },
   {
     name: "Jackson storm",
     type: "racers",
@@ -263,15 +263,44 @@ $(".main__filter-btn").each(function (index, el) {
     });
     $(this).addClass("active");
     filter = $(this).attr("id");
-    changeTab(filter);
     countCars = 6;
+    changeTab(filter, countCars);
+    if(countCars>=cars.filter((car) => car.type == filter.slice(4) || filter.slice(4) == "all").length){
+      showMoreBtn[0].style.display='none';
+      hideBtn[0].style.display='none';
+      btnBox[0].style.flexDirection='row-reverse';
+    }else{
+      hideBtn[0].style.display='none';
+      showMoreBtn[0].style.display='block';
+      btnBox[0].style.flexDirection='row';
+    }
   });
 });
 
 
 
 let showMoreBtn = $("#btn-show-more");
+let btnBox = $('.show-more-btns');
+let hideBtn = $("#btn-hide");
 let countCars = 6;
 showMoreBtn.on("click", function () {
   changeTab(filter, countCars+=6);
+  let filterType = filter.slice(4);
+  if(countCars<=12){
+    hideBtn[0].style.display='block';
+  }
+  if(countCars>=cars.filter((car) => car.type == filterType || filterType == "all").length){
+    showMoreBtn[0].style.display='none';
+  }
+})
+hideBtn.on("click", function () {
+  if(countCars>12){
+    changeTab(filter, countCars-=6);
+  }else{
+    changeTab(filter, countCars=6);
+    hideBtn[0].style.display='none';
+  }
+  if(showMoreBtn[0].style.display=='none'){
+    showMoreBtn[0].style.display='block';
+  }
 })
