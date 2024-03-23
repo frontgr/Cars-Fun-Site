a686144a5b35e097bcf71e16302726d9f2b76c9e
nibezo <nibezo.cs@gmail.com>
2024-03-23 23:55:50 +0400

    feat: Add black bgc for popup

--- front/js/popup.js
+++ front/js/popup.js
@@ -2,8 +2,10 @@ let popupWindow = $("#popupWindow");
 
 function hangePhotoButtonClick(event) {
   if (popupWindow.css("display") == "none") {
+    document.getElementById("overlay").style.display = "block";
     popupWindow.css("display", "flex");
   } else {
+    document.getElementById("overlay").style.display = "none";
     popupWindow.css("display", "none");
   }
 }
@@ -15,82 +17,81 @@ $(document).on("click", function (e) {
       $(e.target).closest(".btn-photo").length > 0
     )
   ) {
+    document.getElementById("overlay").style.display = "none";
     $("#popupWindow").hide();
   }
 });
 
-
 let asideItems = $(".aside__item");
-asideItems.each(function(index, el){
-  $(el).on("click", function(){
-    asideItems.each(function(index, el){
+asideItems.each(function (index, el) {
+  $(el).on("click", function () {
+    asideItems.each(function (index, el) {
       $(el).removeClass("active");
-    })
+    });
     $(this).addClass("active");
-    $("section").each(function(index, value){
+    $("section").each(function (index, value) {
       $(value).removeClass("active");
-    })
+    });
     $("section").eq(index).addClass("active");
-  })
-})
+  });
+});
 
-
-
-$('.popup__close').each(function(index, value){
-  $(value).on("click",closePopup);
-})
+$(".popup__close").each(function (index, value) {
+  $(value).on("click", closePopup);
+});
 let editCarPopup = $("#edit-car-popup");
 let editUserPopup = $("#edit-user-popup");
 function handleEditBtn(element) {
-    if (element.css("display") == "none") {
-      element.css("display", "flex");
-    }
-  
+  if (element.css("display") == "none") {
+    element.css("display", "flex");
+  }
 }
 
 let editCarBtns = $(".edit-car-btn");
-editCarBtns.each(function(index, el){
-  $(el).on("click", ()=>handleEditBtn($('#edit-car-popup')));
-})
-
+editCarBtns.each(function (index, el) {
+  $(el).on("click", () => handleEditBtn($("#edit-car-popup")));
+});
 
 let editUserBtns = $(".edit-user-btn");
-editUserBtns.each(function(index, el){
-  $(el).on("click", ()=>handleEditBtn($('#edit-user-popup')));
-})
+editUserBtns.each(function (index, el) {
+  $(el).on("click", () => handleEditBtn($("#edit-user-popup")));
+});
 
-let popaps = ['#edit-car-popup', '#add-car-popup', '#edit-user-popup', '#user-popup','#delete-popup'];
+let popaps = [
+  "#edit-car-popup",
+  "#add-car-popup",
+  "#edit-user-popup",
+  "#user-popup",
+  "#delete-popup",
+];
 
 function closePopup() {
-  popaps.forEach(popup => {
+  popaps.forEach((popup) => {
     $(popup).css("display", "none");
-  })
+  });
 }
 
 let addCarBtn = $("#add-car-btn");
-addCarBtn.on("click", ()=>handleEditBtn($('#add-car-popup')));
+addCarBtn.on("click", () => handleEditBtn($("#add-car-popup")));
 
 let addUserBtn = $("#add-user-btn");
-addUserBtn.on("click", ()=>handleEditBtn($('#user-popup')));
-
-
+addUserBtn.on("click", () => handleEditBtn($("#user-popup")));
 
 let deleteCarBtns = $(".delete-car-btn");
-deleteCarBtns.each(function(index, el){
-  $(el).on("click", ()=>handleEditBtn($('#delete-popup')));
-})
+deleteCarBtns.each(function (index, el) {
+  $(el).on("click", () => handleEditBtn($("#delete-popup")));
+});
 
 let deleteUserBtns = $(".delete-user-btn");
-deleteUserBtns.each(function(index, el){
-  $(el).on("click", ()=>handleEditBtn($('#delete-popup')));
-})
-
+deleteUserBtns.each(function (index, el) {
+  $(el).on("click", () => handleEditBtn($("#delete-popup")));
+});
 
 /* eye change */
 let eyeBtns = $(".eye");
-eyeBtns.each((index, value)=>{
-  $(value).on("click", ()=>{
+eyeBtns.each((index, value) => {
+  $(value).on("click", () => {
     $(value).toggleClass("active");
-  })
-})
+  });
+});
 /* eye change */
