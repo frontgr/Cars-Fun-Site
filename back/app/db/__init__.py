afce2e93c745a155525a2468d0736ae5a94a6b6f
reydotcom <rey.dot.conn@gmail.com>
2024-04-16 11:55:57 +0200

    refactor: __init__ files have been added to reduce the length of import paths

---
+++ back/app/db/__init__.py
@@ -0,0 +1,3 @@
+from .current_admin import CurrentAdmin
+from .admin_operations import AdminOperations
+from .car_operations import CarOperations
---
+++ back/app/modules/decorators/__init__.py
@@ -0,0 +1,2 @@
+from .admin_permission_check import check_admin_permission
+from .file_extension_check import check_file_extension
