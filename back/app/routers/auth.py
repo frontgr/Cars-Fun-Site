8d96274eca1ba84f0cb29e0e951b1f14b3275313
reydotcom <rey.dot.conn@gmail.com>
2024-04-16 12:12:22 +0200

    refactor: Unnecessary CSRF token creation has been removed, and the import has been changed accordingly

--- back/app/routers/auth.py
+++ back/app/routers/auth.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, set_access_cookies
-from flask_wtf.csrf import generate_csrf
 
-from app.models.admin import Admin
+from ..db import CurrentAdmin
 
 
 auth = Blueprint('auth', __name__)
@@ -14,18 +13,15 @@ def login_post():
     password = request.form.get('password')
 
     try:
-        admin = Admin(login)
+        admin = CurrentAdmin(login)
 
         if admin.data_validation(password) is False:
             return jsonify({"msg": "The data did not pass validation"}), 400
     except AttributeError:
         return '', 400
 
-    response_data = jsonify({"msg": "Login successful"})
+    response = jsonify({"msg": "Login successful"})
     access_token = create_access_token(identity=admin.get_id())
-    csrf_token = generate_csrf()
+    set_access_cookies(response, access_token)
 
-    set_access_cookies(response_data, access_token)
-    response_data.set_cookie('csrf_access_token', csrf_token)
-
-    return response_data
+    return response
