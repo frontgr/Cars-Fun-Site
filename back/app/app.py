a9cf362db1f4e88c7f4b75e1b581a57358078a83
reydotcom <rey.dot.conn@gmail.com>
2024-04-16 12:06:16 +0200

    refactor: Import paths have been changed, the JWT token refresh function has been moved, and router names have been modified

--- back/app/app.py
+++ back/app/app.py
@@ -1,17 +1,15 @@
-from datetime import timedelta
+from datetime import datetime, timedelta, timezone
 
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_jwt_extended import get_jwt, create_access_token, get_jwt_identity, set_access_cookies
 
 from .config import secret_key, jwt_secret_key
-from .response_handlers import refresh_expiring_jwts
 
-from .models.admin import Admin
+from .db import CurrentAdmin
 
-from .routers.main import main
-from .routers.auth import auth
-from .routers.panel import panel
-from .routers.admins import admins
+from .routers import public, auth
+from .routers.admin_panel import admins_panel, cars_panel
 
 app = Flask(__name__)
 app.secret_key = secret_key
@@ -24,18 +22,27 @@ jwt = JWTManager(app)
 
 
 @app.after_request
-def after_request_handler(response):
-    return refresh_expiring_jwts(response)
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        return response
 
 
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
-    return Admin(_id=identity)
+    return CurrentAdmin(_id=identity)
 
 
-app.register_blueprint(main)
+app.register_blueprint(public)
 app.register_blueprint(auth)
-app.register_blueprint(panel)
-app.register_blueprint(admins)
+app.register_blueprint(admins_panel)
+app.register_blueprint(cars_panel)
 
