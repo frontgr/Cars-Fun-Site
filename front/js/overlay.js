06f9aca943f79728ccf37f7801c45e0c2699c8a7
AlekseySteinberg <117911432+AleksSteinberg@users.noreply.github.com>
2024-04-28 16:21:37 +0400

    Feat: add function slider. Fix: Fix margin, gap and image size

---
+++ front/js/overlay.js
@@ -0,0 +1,80 @@
+let popupWindow = $("#popupWindow");
+
+function hangePhotoButtonClick(event) {
+  if (popupWindow.css("display") == "none") {
+    document.getElementById("overlay").style.display = "block";
+    popupWindow.css("display", "flex");
+  } else {
+    document.getElementById("overlay").style.display = "none";
+    popupWindow.css("display", "none");
+  }
+}
+$(".popup__slider-close").on("click", function (e) {
+  document.getElementById("overlay").style.display = "none";
+  popupWindow.css("display", "none");
+});
+$(document).on("click", function (e) {
+  if (
+    !(
+      $(e.target).closest("#popupWindow").length > 0 ||
+      $(e.target).closest(".btn-photo").length > 0
+    )
+  ) {
+    document.getElementById("overlay").style.display = "none";
+    $("#popupWindow").hide();
+  }
+});
+
+// Переключение выделения картинок в слайдере + замена главной картинки
+
+$(document).ready(function() {
+  
+  // Пролистывание вправо
+
+  $(".popup-menu__right").click(function() {
+    let currentImage = $('.popup-menu__icons .popup-menu__icon--selected');
+    let nextImage = currentImage.next('img');
+
+    if (nextImage.length) {
+      currentImage.removeClass('popup-menu__icon--selected');
+      nextImage.addClass('popup-menu__icon--selected');
+      let imagePath = nextImage.attr('src');
+      $('.popup__main-photo').attr('src', imagePath);
+    } else {
+      $('.popup-menu__icons img:first').addClass('popup-menu__icon--selected');
+      currentImage.removeClass('popup-menu__icon--selected');
+      let firstImagePath = $('.popup-menu__icons img:first').attr('src');
+      $('.popup__main-photo').attr('src', firstImagePath);
+    }
+  });
+
+  // Пролистывание влево
+
+  $(".popup-menu__left").click(function() {
+    let currentImage = $('.popup-menu__icons .popup-menu__icon--selected');
+    let prevImage = currentImage.prev('img');
+
+    if (prevImage.length) {
+      currentImage.removeClass('popup-menu__icon--selected');
+      prevImage.addClass('popup-menu__icon--selected');
+      let imagePath = prevImage.attr('src');
+      $('.popup__main-photo').attr('src', imagePath);
+    } else {
+      $('.popup-menu__icons img:last').addClass('popup-menu__icon--selected');
+      currentImage.removeClass('popup-menu__icon--selected');
+      let lastImagePath = $('.popup-menu__icons img:last').attr('src');
+      $('.popup__main-photo').attr('src', lastImagePath);
+    }
+  });
+
+  // По нажататию на картинку
+  
+  $(".popup-menu__icon").click(function() {
+    let imagePath = $(this).attr('src');
+    $('.popup-menu__icons .popup-menu__icon--selected').removeClass('popup-menu__icon--selected');
+    $(this).addClass('popup-menu__icon--selected');
+    $('.popup__main-photo').attr('src', imagePath);
+  });
+});
+
+
