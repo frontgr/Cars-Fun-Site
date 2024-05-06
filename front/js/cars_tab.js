96ee33aecd275d501b544692e50f0e469c79b2a4
nibezo <nibezo.cs@gmail.com>
2024-05-06 13:41:44 +0400

    feat: Add search for the main page

--- front/js/cars_tab.js
+++ front/js/cars_tab.js
@@ -1,162 +1,4 @@
-export const cars = [
-    {
-        name: "Jackson storm",
-        type: "racers",
-        number: 20,
-        photo: "./images/main_item_3.webp",
-    },
-    {
-        name: "Lewis Hamilton",
-        type: "racers",
-        photo: "./images/main_item_4.webp",
-    },
-    {
-        name: "Metr",
-        type: "other",
-        photo: "./images/main_item_5.webp",
-    },
-    {
-        name: "Luigi",
-        type: "other",
-        photo: "./images/main_item_6.webp",
-    },
-    {
-        name: "Cruz Ramirez",
-        type: "racers",
-        photo: "./images/main_item_2.webp",
-    },
-    {
-        name: "Lightning McQueen",
-        number: 95,
-        type: "racers",
-        photo: "./images/main_item_1.webp",
-    },
-    {
-        name: "Cruz Ramirez",
-        type: "racers",
-        photo: "./images/main_item_2.webp",
-    },
-    {
-        name: "Lightning McQueen",
-        number: 95,
-        type: "racers",
-        photo: "./images/main_item_1.webp",
-    },
-    {
-        name: "Cruz Ramirez",
-        type: "racers",
-        photo: "./images/main_item_2.webp",
-    },
-    {
-        name: "Lightning McQueen",
-        number: 95,
-        type: "racers",
-        photo: "./images/main_item_1.webp",
-    },
-    {
-        name: "Jackson storm",
-        type: "racers",
-        number: 20,
-        photo: "./images/main_item_3.webp",
-    },
-    {
-        name: "Lewis Hamilton",
-        type: "racers",
-        photo: "./images/main_item_4.webp",
-    },
-    {
-        name: "Metr",
-        type: "other",
-        photo: "./images/main_item_5.webp",
-    },
-    {
-        name: "Luigi",
-        type: "other",
-        photo: "./images/main_item_6.webp",
-    },
-    {
-        name: "Cruz Ramirez",
-        type: "racers",
-        photo: "./images/main_item_2.webp",
-    },
-    {
-        name: "Lightning McQueen",
-        number: 95,
-        type: "racers",
-        photo: "./images/main_item_1.webp",
-    },
-    {
-        name: "Cruz Ramirez",
-        type: "racers",
-        photo: "./images/main_item_2.webp",
-    },
-    {
-        name: "Lightning McQueen",
-        number: 95,
-        type: "racers",
-        photo: "./images/main_item_1.webp",
-    },
-    {
-        name: "Cruz Ramirez",
-        type: "racers",
-        photo: "./images/main_item_2.webp",
-    },
-    {
-        name: "Lightning McQueen",
-        number: 95,
-        type: "racers",
-        photo: "./images/main_item_1.webp",
-    },
-    {
-        name: "Jackson Storm",
-        type: "racers",
-        number: 20,
-        photo: "./images/main_item_3.webp",
-    },
-    {
-        name: "Lewis Hamilton",
-        type: "racers",
-        photo: "./images/main_item_4.webp",
-    },
-    {
-        name: "Metr",
-        type: "other",
-        photo: "./images/main_item_5.webp",
-    },
-    {
-        name: "Luigi",
-        type: "other",
-        photo: "./images/main_item_6.webp",
-    },
-    {
-        name: "Cruz Ramirez",
-        type: "racers",
-        photo: "./images/main_item_2.webp",
-    },
-    {
-        name: "Lightning McQueen",
-        number: 95,
-        type: "racers",
-        photo: "./images/main_item_1.webp",
-    },
-    {
-        name: "Cruz Ramirez",
-        type: "racers",
-        photo: "./images/main_item_2.webp",
-    },
-    {
-        name: "Lightning McQueen",
-        number: 95,
-        type: "racers",
-        photo: "./images/main_item_1.webp",
-    },
-    {
-        name: "Cruz Ramirez",
-        type: "racers",
-        photo: "./images/main_item_2.webp",
-    },
-];
-
+import { cars } from "./cars_list.js";
 function changeTab(filter, countCars = 6) {
     let content__cars = "";
     let filterType = filter.slice(4);
@@ -173,16 +15,16 @@ function changeTab(filter, countCars = 6) {
             <div class="main__item-title car-item__title">
                 ${car.name} <span>${car.number ? "#" + car.number : "~"}</span>
             </div>
-            <div class="item__btns">
+            <div class="car-item__btns">
                 <button
-                    class="item__btns-btn btn-photo"
+                    class="car-item__btns-btn btn-photo"
                     onclick="hangePhotoButtonClick()"
                 >
                     Photo
                 </button>
                 <a
                     href="./pages/car_info.html"
-                    class="item__btns-btn btn-specifications"
+                    class="car-item__btns-btn btn-specifications"
                 >
                     Specifications
                 </a>
--- front/js/search.js
+++ front/js/search.js
@@ -1,16 +1,65 @@
-const cars = window.cars;
-let inputField = document.getElementById("searchInput");
-let inputButton = document.getElementById("searchInputButton");
-
-console.log(cars);
-function search() {
-    if (inputField.value.length >= 3) {
+import { cars } from "./cars_list.js";
+window.search = function search() {
+    $(".header__search").removeClass("header__search--error");
+    let inputField = document.getElementById("searchInput");
+    if (inputField.value.length >= 3 && inputField.value.length <= 256) {
+        $(".header__search").removeClass("header__search--error");
         $(".search-block").show();
+        inputField = inputField.value;
+        let searchContent = "";
+        $(".search-block__not-found").hide();
+        cars.forEach((car) => {
+            if (car.name.toLowerCase().includes(inputField.toLowerCase())) {
+                console.log(car.name);
+                searchContent += `
+                <div class="search-block__item car-item">
+                <img
+                    src=${car.photo}
+                    alt="car image"
+                    class="car-item__img"
+                />
+                <div class="car-item__title">
+                ${car.name} <span>${car.number ? "#" + car.number : "~"}</span>
+                </div>
+                <div class="car-item__btns">
+                    <button
+                        class="car-item__btns-btn btn-photo"
+                        onclick="hangePhotoButtonClick()"
+                    >
+                        Photo
+                    </button>
+                    <a
+                        href="./pages/car_info.html"
+                        class="car-item__btns-btn btn-specifications"
+                    >
+                        Specifications
+                    </a>
+                </div>
+            </div>
+                `;
+            }
+            $(".search-block__container").html(searchContent);
+        });
+        if (searchContent === "") {
+            $(".search-block__not-found").show();
+        }
     } else {
+        $(".header__search").addClass("header__search--error");
         $(".search-block").hide();
     }
-}
+};
 
-function closeSearch() {
+window.closeSearch = function closeSearch() {
+    $(".header__search").removeClass("header__search--error");
     $(".search-block").hide();
-}
+};
+
+window.removeError = function removeError() {
+    $(".header__search").removeClass("header__search--error");
+};
+
+$(".header__search").keydown(function (event) {
+    if (event.keyCode === 13) {
+        search();
+    }
+});
