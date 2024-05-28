6b55d9e10f010acadd932482e188e1be49a4fb64
reydotcom <rey.dot.conn@gmail.com>
2024-05-28 08:45:15 +0200

    fix: Storage path was changed

--- back/app/config.py
+++ back/app/config.py
@@ -6,7 +6,7 @@ secret_key = os.environ['SECRET_KEY']
 jwt_secret_key = os.environ['JWT_SECRET_KEY']
 
 # Storage
-storage = './storage'
+storage = '/Cars-Fun-Site/photo'
 extensions = {'png', 'jpg', 'jpeg', 'webp'}
 
 # DataBase
