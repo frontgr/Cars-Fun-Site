f90c1af6dc01337b38d36dce04c783257e1e2aa5
reydotcom <rey.dot.conn@gmail.com>
2024-05-28 10:05:51 +0200

    fix: Arrays storing the names of the model fields were removed due to being unnecessary

--- back/app/config.py
+++ back/app/config.py
@@ -11,21 +11,3 @@ extensions = {'png', 'jpg', 'jpeg', 'webp'}
 
 # DataBase
 db_uri = os.environ['DB_URI']
-
-# Models
-add_admin_fields = {'login',
-                    'password',
-                    'add_users',
-                    'edit_users',
-                    'delete_users',
-                    'add_cars',
-                    'edit_cars',
-                    'delete_cars'}
-
-update_admin_fields = {'_id',
-                       'add_users',
-                       'edit_users',
-                       'delete_users',
-                       'add_cars',
-                       'edit_cars',
-                       'delete_cars'}
