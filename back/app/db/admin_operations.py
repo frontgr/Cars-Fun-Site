7e70cfc62ca6f5fbd5025180d5338267e60cbc73
reydotcom <rey.dot.conn@gmail.com>
2024-06-27 10:31:18 +0200

    feat: Database interaction is now performed through classes with database models

--- back/app/db/admin_operations.py
+++ back/app/db/admin_operations.py
@@ -1,39 +1,33 @@
-from bson.objectid import ObjectId
-
 from werkzeug.security import generate_password_hash
 
-from .client import db
+from .models import Admin
 
 
 class AdminOperations:
     @staticmethod
     def get_admins():
-        admins_dict = {}
+        admins = {}
 
-        result = db.admins.find({}, {'password': False})
-        for index, item in enumerate(result):
-            admins_dict[index] = {
-                key: (str(value) if key == "_id" else value) for key, value in item.items()}
+        for index, item in enumerate(Admin.objects.exclude('password')):
+            admins[index] = item.to_mongo()
             
-        return admins_dict
+        return admins
 
     @staticmethod
     def add_admin(values):
         values['password'] = generate_password_hash(values['password'])
 
-        db.admins.insert_one(values)
+        Admin(**values).save()
 
     @staticmethod
-    def delete_admin(_id):
-        db.admins.delete_one({'_id': ObjectId(_id)})
+    def delete_admin(id):
+        Admin.objects(id=id).delete()
 
     @staticmethod
-    def update_admin(_id, values_dict):
+    def update_admin(id, values_dict):
         try:
             values_dict['password'] = generate_password_hash(values_dict['password'])
         except KeyError:
             pass
         
-        db.admins.update_one(
-            {'_id': ObjectId(_id)}, 
-            {'$set': values_dict})
+        Admin.objects(id=id).modify(**values_dict)
--- back/app/db/car_operations.py
+++ back/app/db/car_operations.py
@@ -1,39 +1,24 @@
-from bson.objectid import ObjectId
-
+import re
 import uuid
 
 from ..modules.converter import convert, delete_storage
 
-from .client import db
+from .models import Cars
 from ..modules.decorators import check_file_extension
 
 
 class CarOperations:
     @staticmethod
     def get_cars():
-        cars_dict = {}
-        for index, item in enumerate(db.cars.find()):
-            temp = {}
-            for key, value in item.items():
-                if key == '_id':
-                    temp[key] = str(value)
-                    continue
-                temp[key] = value
-            cars_dict[index] = temp
-        return cars_dict
+        cars = {}
+        for index, item in enumerate(Cars.objects()):
+            cars[index] = item.to_mongo()
+        return cars
 
     @staticmethod
-    def get_car(_id):
-        car_dict = {}
-        response = db.cars.find_one({'_id': ObjectId(_id)})
-
-        for key, value in response.items():
-            if key == '_id':
-                car_dict[key] = str(value)
-                continue
-            car_dict[key] = value
-
-        return car_dict
+    def get_car(id):
+        car = Cars.objects(id=id).first()
+        return car.to_mongo()
 
     @staticmethod
     @check_file_extension
@@ -43,25 +28,27 @@ class CarOperations:
         values_dict['folder_id'] = folder_id
 
         for key, value in photos.items():
-            values_dict[key] = convert(folder_id=folder_id, index=key, file=value)
+            if re.fullmatch(rf'{key}', r'photo\d'):
+                values_dict['photos'] = {key: convert(folder_id=folder_id, index=key, file=value)}
+            else:
+                values_dict[key] = convert(folder_id=folder_id, index=key, file=value)
 
-        db.cars.insert_one(values_dict)
+        car = Cars(**values_dict)
+        car.save()
 
     @staticmethod
-    def delete_car(_id):
-        folder_id = db.cars.find_one({'_id': ObjectId(_id)})['folder_id']
+    def delete_car(id):
+        folder_id = Cars.objects(id=id).first().folder_id
 
         delete_storage(folder_id)
-        db.cars.delete_one({'_id': ObjectId(_id)})
+        Cars.objects(id=id).delete()
 
     @staticmethod
     @check_file_extension
-    def update_car(_id, values_dict, photos):
-        folder_id = db.cars.find_one({'_id': ObjectId(_id)})['folder_id']
+    def update_car(id, values_dict, photos):
+        folder_id = Cars.objects(id=id).first().folder_id
 
         for key, value in photos.items():
             values_dict[key] = convert(folder_id=folder_id, index=key, file=value)
 
-        db.cars.update_one(
-            {'_id': ObjectId(_id)}, 
-            {'$set': values_dict})
+        Cars.objects(id=id).modify(**values_dict)
