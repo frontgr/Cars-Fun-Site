1957012af45e6d9803529d2778d8c13daeb29cb2
reydotcom <rey.dot.conn@gmail.com>
2024-06-27 10:24:45 +0200

    feat: Fetching root admin data from environment variables was added

--- back/app/config.py
+++ back/app/config.py
@@ -1,6 +1,5 @@
 import os
 
-
 # Flask app
 secret_key = os.environ['SECRET_KEY']
 jwt_secret_key = os.environ['JWT_SECRET_KEY']
@@ -11,3 +10,7 @@ extensions = {'png', 'jpg', 'jpeg', 'webp'}
 
 # DataBase
 db_uri = os.environ['DB_URI']
+
+#Root admin data
+root_admin_login = os.environ['ROOT_LOGIN']
+root_admin_password = os.environ['ROOT_PASSWORD']
