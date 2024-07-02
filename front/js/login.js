5efc2191751f4bf1d3131d3786391e70ba59ab00
reydotcom <rey.dot.conn@gmail.com>
2024-07-02 14:34:29 +0200

    fix: The script was modified for correct authentication and cookie retrieval

--- front/js/login.js
+++ front/js/login.js
@@ -1,35 +1,34 @@
-function getCookie(name) {
-    const value = document.cookie;
-    const parts = value.split(`${name}=`);
-    if (parts.length === 2) return parts.pop().split(";").shift();
-}
-
-const BACKEND_URL = "http://127.0.0.1:5000";
-const TOKEN = getCookie("csrf_access_token");
-
-async function getResource(url, method, body) {
-    const myHeaders = new Headers();
-    myHeaders.append("X-CSRF-TOKEN", getCookie("csrf_access_token"));
+async function sendLoginRequest(formData) {
     const options = {
-        method: method,
-        headers: myHeaders,
+        method: "POST",
+        credentials: 'include',
+        body: formData
     };
-    const res = await fetch(BACKEND_URL + url, options);
+
+    const res = await fetch("http://127.0.0.1:5000/login", options);
+
     if (!res.ok) {
-        throw new Error(`Could not fetch ${url}, received ${res.status}`);
-    }
-    return await res.json();
-}
-
-async function getCars() {
-    try {
-        const result = await getResource("/panel/cars", "GET");
-        console.log(result);
-        return result;
-    } catch {
-        throw new Error();
+        const errorMessage = await res.text();
+        throw new Error(`${errorMessage}`);
+    } else {
+        window.location.href = './admin.html';
     }
 }
 
-console.log(document.cookie);
-getCars();
+function makeRequest() {
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var login = document.getElementById('login').value;
+        var password = document.getElementById('password').value;
+        
+        const formData = new FormData();
+
+        formData.append('login', login)    
+        formData.append('password', password)
+
+        sendLoginRequest(formData);
+    });
+}
+
+makeRequest();
